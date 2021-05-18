aa895a464d4d7e5664939dc72bc4670a71a49d5e
johnhoth <zharov.ia@phystech.edu>
2021-05-18 14:31:09 +0300

    commiting config DANGEROUS

---
+++ src/config.py
@@ -0,0 +1,9 @@
+API_SECRET_KEY = 'STUPID'
+
+url = 'https://digitaltamagochi.herokuapp.com/'
+check_connection_url = 'https://digitaltamagochi.herokuapp.com/connect'
+login_url = 'https://digitaltamagochi.herokuapp.com/login'
+register_url = 'https://digitaltamagochi.herokuapp.com/register'
+jest_list_url = 'https://digitaltamagochi.herokuapp.com/jestList'
+jest_add_url = 'https://digitaltamagochi.herokuapp.com/jestAdd'
+jest_change_url = 'https://digitaltamagochi.herokuapp.com/jestChangeValue'
